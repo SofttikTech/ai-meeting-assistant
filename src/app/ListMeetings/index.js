c9cea2ac8192dc36a4065c470bf1bb1a0cce4677
bitgrav <bitgravpk@gmial.com>
2025-02-27 12:40:08 +0500

    animation add and pages set

--- src/app/ListMeetings/index.js
+++ src/app/ListMeetings/index.js
@@ -19,7 +19,7 @@ function ListMeetings() {
   const [previousClients, setPreviousClients] = useState([]);
   const [modalProfileAdvisor, setModalProfileAdvisor] = useState(false);
   const [isVisibleMeetingDetail, setIsVisibleMeetingDetail] = useState(false);
-  const [isVisibleMeetingDetailPrevious, setIsVisibleMeetingDetailPrevious] = useState(true);
+  const [isVisibleMeetingDetailPrevious, setIsVisibleMeetingDetailPrevious] = useState(false);
   const [profileData, setProfileData] = useState("");
 
   const handleLogout = () => {
