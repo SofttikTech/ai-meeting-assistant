b0ca2b31a393df230b3eb6a53dd1847937bfd5bf
Haris Ellahi <981738844hariscoder3@gmail.com>
2025-04-08 08:50:04 +0500

    updated-response

--- src/app/ListMeetings/meetingAssistant.js
+++ src/app/ListMeetings/meetingAssistant.js
@@ -307,7 +307,7 @@ const Talk = ({ setIsVisibleAssistant }) => {
             );
             sendAudioToBackend();
           }
-        }, 20000);
+        }, 10000);
       } catch (error) {
         console.error("Error starting recording:", error);
         setIsRecording(false);
