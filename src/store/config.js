14dc2ed959def0bb206d30fe12fbbecc3c79c8d3
Haris Ellahi <981738844hariscoder3@gmail.com>
2025-04-30 08:54:21 +0500

    updated-links

--- src/store/config.js
+++ src/store/config.js
@@ -1,8 +1,8 @@
-const BASE_URL = 'http://127.0.0.1:4000';
-// const BASE_URL = 'https://database.epiphanyadvisor.com';
+// const BASE_URL = 'http://127.0.0.1:4000';
+const BASE_URL = 'https://database.epiphanyadvisor.com';
 
-// export const URL = 'https://server.epiphanyadvisor.com';
-export const URL = 'http://127.0.0.1:5000';
+export const URL = 'https://server.epiphanyadvisor.com';
+// export const URL = 'http://127.0.0.1:5000';
 
 // Generic API call helper
 const apiCall = async (url, method = 'GET', data = null, extraOptions = {}) => {
