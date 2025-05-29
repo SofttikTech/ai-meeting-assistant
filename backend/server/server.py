99e52467f834302466783e696393efe2fd3a9f45
Haris Ellahi <harisellahi888@gmail.com>
2025-05-29 14:41:08 +0500

    update

--- backend/server/server.py
+++ backend/server/server.py
@@ -426,5 +426,4 @@ def handle_disconnect():
 
 if __name__ == "__main__":
     logging.info("Starting server on port 5000")
-    socketio.run(app, host="0.0.0.0", port=5000, debug=False) 
-    # allow_unsafe_werkzeug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=False, allow_unsafe_werkzeug=True)
